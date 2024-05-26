06d1291f902bae35394462b002556b6e85759de9
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-05-26 14:01:36 +0500

    this is change to arrys folder

---
+++ arrysMehtods2.js
@@ -0,0 +1,11 @@
+let mohib =['name','age','reg','number','sem','sim','polls'];
+let chk = mohib.indexOf('age');
+console.log(chk);
+
+let mohib1 =['name','age','reg','number','sem','sim','polls'];
+let chk1 = mohib1.lastIndexOf();
+console.log(chk1);
+
+let mohib2 =['name','age','reg','number','sem','sim','polls'];
+let chk2 = mohib2.findLast();
+console.log(chk2);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,6 +1,3 @@
-let boys =[ 'mohib','khan','mahsud','me'];
-let girls =['jk','lo','kp ','mo'];
-let gays =['hehe','ueue','ieioei','ekjeje']
-let bh = boys.concat(girls,gays,'this is string pass as argumnet',909909);
-// boys.concat(girls);
-console.log(bh);
+let mohib2 =['name','age','reg','number','sem','sim','polls'];
+let chk2 = mohib2.findLast();
+console.log(chk2);
