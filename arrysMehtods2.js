5ba5a1834848b10e80ec05613639202f3f440e2c
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-05-27 18:12:37 +0500

    modiefid arrays and sear()

--- arrysMehtods2.js
+++ arrysMehtods2.js
@@ -15,3 +15,13 @@ let value1 = ['mohib','khan','mahsud','age'];
 console.log(value1.includes('min'));
 
 //ok so incluse mthdi is one of the best to find out element in array and this is best thing to find lost values and check for it 
+
+//lets try out find() function with use of function and return value using arguments in functions 
+
+const number =[12,10,9,20,25,30,40];
+let first = number.find(myFunction);
+console.log('it can return the greater value in array',first);
+function myFunction(v,i,a){
+// so writung value and and index array just for represenation of arguments 
+return v > 30;
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,7 @@
-let value1 = ['mohib','khan','mahsud','age'];
-console.log(value1.includes('min'));
+const number =[12,10,9,20,25,30,40];
+let first = number.find(myFunction);
+console.log('it can return the greater value in array',first);
+function myFunction(v,i,a){
+
+return v > 30;
+}
