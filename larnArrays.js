d4522dcaa218b4176f2aeb8a8fed52e7bdadf0d2
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-05-29 23:59:43 +0500

    more

--- larnArrays.js
+++ larnArrays.js
@@ -1,3 +1,6 @@
 // i implement so many search( ) arrays methods and learned so many new things 
 const mohib = [ 'name ','age ,roll no '];
 console.log(mohib.indexOf('name'));
+
+let num = [32323,3232,32323,4434,434,2,2,45,5,55,5,5,5,5];
+console.log(num.length);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,2 @@
-const mohib = [ 'name ','age ,roll no '];
-console.log(mohib.indexOf('name'));
+let num = [32323,3232,32323,4434,434,2,2,45,5,55,5,5,5,5];
+console.log(num.length);
